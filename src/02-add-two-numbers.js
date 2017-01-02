23923e14b103918c0b7d5e40a03d32b6952599c8
maximderbin <maxim@derbin.io>
2017-01-02 12:58:28 -0600

    Fix style

--- src/02-add-two-numbers.js
+++ src/02-add-two-numbers.js
@@ -1,7 +1,9 @@
 /**
  * https://leetcode.com/problems/add-two-numbers/
  *
- * You are given two linked lists representing two non-negative numbers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+ * You are given two linked lists representing two non-negative numbers.
+ * The digits are stored in reverse order and each of their nodes contain a single digit.
+ * Add the two numbers and return it as a linked list.
  *
  * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
  * Output: 7 -> 0 -> 8
@@ -11,31 +13,38 @@
  *     this.val = val;
  *     this.next = null;
  * }
- */
-/**
+ *
+ *
  * @param {ListNode} l1
  * @param {ListNode} l2
  * @return {ListNode}
+ *
+ * var addTwoNumbers = function(l1, l2) {
+ *
+ * };
  */
-function addTwoNumbers(l1, l2) {
-  let sum = 0
-  let modulo = 0
-  let current = new ListNode()
-  const head = current
+
+var ListNode = require('./utils/ListNode');
+
+module.exports = function(l1, l2) {
+  let sum = 0;
+  let modulo = 0;
+  let current = new ListNode();
+  const head = current;
 
   while (l1 || l2 || modulo) {
     if (typeof current.val === 'number') {
-      current.next = new ListNode()
-      current = current.next
+      current.next = new ListNode();
+      current = current.next;
     }
 
-    sum = (l1 && l1.val || 0) + (l2 && l2.val || 0) + modulo
-    modulo = Math.floor(sum / 10)
-    current.val = sum % 10
+    sum = (l1 && l1.val || 0) + (l2 && l2.val || 0) + modulo;
+    modulo = Math.floor(sum / 10);
+    current.val = sum % 10;
 
-    l1 = l1 && l1.next
-    l2 = l2 && l2.next
+    l1 = l1 && l1.next;
+    l2 = l2 && l2.next;
   }
 
-  return head
+  return head;
 }
