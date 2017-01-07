1b9b91f0efd561302a521dde3337ebff09c7da1d
maximderbin <maxim@derbin.io>
2017-01-07 16:18:54 -0600

    238. Product of Array Except Self

---
+++ src/__tests__/238-product-array-except-self-test.js
@@ -0,0 +1,7 @@
+var problem = require('../238-product-array-except-self');
+
+describe('238-product-array-except-self', function() {
+  it('works', function() {
+    expect(problem([1, 2, 3, 4])).toEqual([24, 12, 8, 6]);
+  });
+});
