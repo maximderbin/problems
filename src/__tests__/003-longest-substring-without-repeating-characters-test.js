c9d5a837be463a3dc8d459fa5876a63af84a8d81
Valery Bugakov <skymk1@gmail.com>
2017-01-10 15:20:59 +0400

    3. Fixed test cases

--- src/__tests__/003-longest-substring-without-repeating-characters-test.js
+++ src/__tests__/003-longest-substring-without-repeating-characters-test.js
@@ -2,10 +2,11 @@ var problem = require('../003-longest-substring-without-repeating-characters');
 
 describe('003-longest-substring-without-repeating-characters', function() {
   it('works', function() {
-    expect(problem('abcabcbb')).toEqual('abc');
-    expect(problem('bbbbb')).toEqual('b');
-    expect(problem('pwwkew')).toEqual('wke');
-    expect(problem('c')).toEqual('c');
-    expect(problem('aab')).toEqual('ab');
+    expect(problem('abcabcbb')).toEqual(3);
+    expect(problem('bbbbb')).toEqual(1);
+    expect(problem('pwwkew')).toEqual(3);
+    expect(problem('c')).toEqual(1);
+    expect(problem('aab')).toEqual(2);
+    expect(problem('dvda')).toEqual(3);
   });
 });
