8a4509eb648114daaa2625cee4eae23535e23100
maximderbin <maxim@derbin.io>
2017-01-30 21:49:49 -0600

    277. Find the Celebrity

---
+++ src/leetcode/__tests__/277-find-the-celebrity-test.js
@@ -0,0 +1,8 @@
+var problem = require('../277-find-the-celebrity');
+
+describe('277-find-the-celebrity', function() {
+  it('works', function() {
+    // TODO: add real tests
+    expect(true).toEqual(true);
+  });
+});
